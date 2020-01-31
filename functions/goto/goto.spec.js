679c3d8414054e16ef388d6ffac9736b673128dd
Steven Hicks <steven.j.hicks@gmail.com>
2020-01-31 16:49:54 -0600

    Add tests for all current matches

--- functions/goto/goto.spec.js
+++ functions/goto/goto.spec.js
@@ -4,4 +4,32 @@ describe('interpret', () => {
   it('exists', () => {
     expect(typeof interpret).toEqual('function');
   });
+
+  describe('misses', () => {
+    it.each([['steeeve'], ['monkey']])('%s', value => {
+      const result = interpret(value);
+
+      expect(result).toContain('google.com');
+    });
+  });
+
+  describe('hits', () => {
+    it.each([
+      ['palette', 'https://github.com/artsy/palette'],
+      ['palette pulls', 'https://github.com/artsy/palette/pulls'],
+      ['mp', 'https://github.com/artsy/metaphysics'],
+      ['mp pulls', 'https://github.com/artsy/metaphysics/pulls'],
+      ['github', 'https://github.com'],
+      ['staging', 'https://staging.artsy.net'],
+      ['prod', 'https://artsy.net'],
+      ['mail', 'mail.google.com'],
+      ['calendar', 'calendar.google.com'],
+      ['backlog', 'artsyproduct.atlassian.net'],
+      ['sprint', 'artsyproduct.atlassian.net'],
+    ])('%s', (value, expected) => {
+      const result = interpret(value);
+
+      expect(result).toContain(expected);
+    });
+  });
 });
